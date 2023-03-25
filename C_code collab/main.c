f03dd8a7f0e64fb05035191f8638e9a8a2ceee79
Stephen Mwangi <62056954+Stevemwa@users.noreply.github.com>
2023-03-25 18:25:27 +0300

    Add files via upload

--- C_code collab/main.c
+++ C_code collab/main.c
@@ -18,16 +18,19 @@
 
 //_______________VAR DECLARATIONS____________________
 int counter = 0;//while 1 loop counter
+int KEY_Count = 0; //key loop adder
+int p=0; // key loop counter
+
 int PASSWORD_LENGTH = 4;
 int press;//Translates actual integer values of the KEYPAD
 int Open_Close_Counter=0;//helps know if the keypad was open or closed
-int Input_Password[5];//password input from the keypad
-int Actual_Password[4]; //password stored on the eeprom
+unsigned int Input_Password[5];//password input from the keypad
+unsigned int Actual_Password[4]; //password stored on the eeprom
 int password_count; //ntaitaftia tuh kitu 
 char buffer[16];   //this variable helps in conversion of integer to string using the itoa() stdlib function
 int value, input, TILT, i=0;
 uint16_t ENTERED_CODE, STORED_CODE;
-int Safe_Status = 0;//0 for locked , 1 for transitioning, 2 for open
+int Safe_Status ;//0 for locked , 1 for transitioning, 2 for open
 
 
 //_______________FUNC DECLARATIONS____________________
@@ -46,8 +49,9 @@ void TILT_ANGLE();
 
 //___________________________________________________LATCH INTERUPT______________________________________________________________
 ISR(INT1_vect){
+	LCD_Clear();
 if (Safe_Status==1) //changes the safe state to OPEN
-{
+{ 
 	Safe_Status=2;
 }
 	
@@ -57,21 +61,18 @@ if (Safe_Status==1) //changes the safe state to OPEN
 //_________________________________________________KEYPAD  INTERUPT______________________________________________________________
 
 ISR(INT0_vect)
-{ Safe_Status=1;
-	LCD_Clear();
-	LCD_Cmd(0x80);
-	LCD_String("Enter Code");
-	LCD_Cmd(0xC0);
-		for(int p=0;p<=4;p++){
-			value = PINB;
-			KEYCHECK();
-			Input_Password[p]=press;
-			_delay_ms(1000);
-				
-			}
+{    
+	if (p==0 && Safe_Status!=4){LCD_Clear(); LCD_String("Enter Code"); LCD_Cmd(0Xc0); LCD_String(":");}
+	if (Safe_Status == 0){Safe_Status = 1;}
 	
-	
-	
+	 if (p<=4){
+	    value = PINB;
+	    KEYCHECK();
+	    Input_Password[p]=press;
+		p++;
+	   }
+	   
+	_delay_ms(100);
 	
 }
 
@@ -80,17 +81,18 @@ ISR(INT0_vect)
 void BIRG(int x){ //1 means flash green 5times & 2==Light green constantly & 3==Light red constantly
 	
   if (x==3){
-	   for (int t=0;t<=5;t++){  
-		    PORTD= (PORTD|0b01000000);
-			_delay_ms(500);
+	   for (int t=0;t<=5;t++){ 
+		    PORTD=PORTD | 0b00100000; 
+		    
+			_delay_ms(1000);
 			PORTD=PORTD & 0b00011111;
 			_delay_ms(500);
 	       }
 		
-	   }else if(x==2){ PORTD= (
-		 PORTD|0b01000000);	
+	   }else if(x==1){ 
+		 	PORTD=PORTD | 0b00100000;
 		  }else{
-			    PORTD=PORTD | 0b00100000;
+			    PORTD= (PORTD|0b01000000);
 				}
 		   
 		   
@@ -114,15 +116,16 @@ int main(void)
 	
    //________________________________________________________LCD____________________________________________________________________
 	   LCD_Init();
+	   
 	
 	/*______________________________ Interrupt setup___________________________ */
 	
 	//___________________________________________________LATCH INTERUPT______________________________________________________________
-		GICR = 1<<INT1; /* Enable INT1*/
-		MCUCR = 1<<ISC01 | 1<<ISC00; /* Trigger INT0 on rising edge */
+		/* Enable INT1*/
+		
 	//_________________________________________________KEYPAD  INTERUPT______________________________________________________________
-		GICR = 1<<INT0; /* Enable INT0*/
-		MCUCR = 1<<ISC01 | 1<<ISC00; /* Trigger INT0 on rising edge */
+		GICR = 1<<INT0 |1<<INT1; /* Enable INT0*/
+		MCUCR = 1<<ISC01 | 1<<ISC00 |1<<ISC11|1<<ISC10; /* Trigger INT0 and INT1 on rising edge */
 		sei(); /* Enable Global Interrupt */
 	
 	
@@ -130,14 +133,14 @@ int main(void)
 	ADC_Init();
 	
     
-    //___________________________________________________________________________________________________________________________
+    //_________________________________________________________________________________________________________________________________________________________
+	Safe_Status=0;
 	Read_EEPROM_Password();
 	
 	
 	
-    while (1) 
-    {  
-//........................................................................................................................................	
+while (1)  {  
+//............................................................................................................................................................	
 //this section confirms if the password is being saved on the eeprom ,it should display only once after reset to confirm if the password is saved ...telemetry
 		if(counter==1){
 			LCD_Clear(); 
@@ -148,120 +151,144 @@ int main(void)
 					_delay_ms(100);
 				}
 				_delay_ms(3000);
+				LCD_Clear();
 	    }
 	  if (counter==100){counter=2;}
 			counter++;
-//''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+//''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''""''''''''''''''''''
 /*__________________________________LIGHTING OF THE BIRG LED BASED ON SAFE STATUS(refer to safe status var declaration)_____________________________________	*/
-	 if (Safe_Status==0){BIRG(3);LCD_Clear(); LCD_String("Enter Code"); LCD_Cmd(0Xc0); LCD_String("_ _ _ _");} 
-     if (Safe_Status==2){LCD_Clear(); LCD_String("Safe: OPEN"); LCD_Cmd(0Xc0); LCD_String("_ _ _ _"); BIRG(2);}
+	 if (Safe_Status==0 && counter==99){BIRG(0);LCD_Clear(); LCD_String("Enter Code"); LCD_Cmd(0Xc0); LCD_String("_ _ _ _"); p=0;} 
+     if (Safe_Status==2 && counter==99){LCD_Clear(); LCD_String("Safe: OPEN"); LCD_Cmd(0Xc0); LCD_String("_ _ _ _"); BIRG(1);}
 /*____________________________________________READS ADC IF THERE IS THEFT______________________________________________________________________________________ */
 	 TILT_ANGLE(); //Checks tilt  Angle
 	 	
 /*_____________________________________________________AFTER PASSWORD INPUT_____________________________________________________________________________________*/	 
-	 if((Input_Password[4]==15) && (Safe_Status==1)){  //it displays the password entered if the X key is pressed Xkey is read as press=15
-		 LCD_Clear();
-		 LCD_Cmd(0x80); //Write on the top lcd row
-		 LCD_String("Entered Code");
-		 LCD_Cmd(0xc0);
-		 LCD_String(":");
-		 for (int i=0;i<=3;i++)
-		 { itoa (Input_Password[i],buffer,10);
-			 LCD_String(buffer);
-			 _delay_ms(100);
-		 }
-		 Input_Password[4]=14;
-		 }
+		 if((p>3) && (Input_Password[4]==15) && (Safe_Status==1)){  //it displays the password entered if the X key is pressed X key is read as press=15
+			 LCD_Clear();
+			 LCD_Cmd(0x80); //Write on the top lcd row
+			 LCD_String("Entered Code");
+			 LCD_Cmd(0xc0);
+			 LCD_String(":");
+			 for (int i=0;i<=3;i++)
+			 { itoa (Input_Password[i],buffer,10);
+				 LCD_String(buffer);
+				 _delay_ms(100);
+			 }
+			 Input_Password[4]=14;
+			  _delay_ms(2000);
+			 }
 	 
-	 if ((Input_Password[4]==14) && (Safe_Status==1))
-	 {
-		 COMPARE_PASSWORD(); 
-		 BIRG(1);
-	 }
+			if ((p>=4) && (Input_Password[4]==14) && (Safe_Status==1))
+			{   p=0;
+				COMPARE_PASSWORD(); 
+			}
 	 
-	 
-	 	
-		
-		
-    }
+}
 }
 
 //_________________________________________________FUNCTIONS__________________________________________________________________________________________________________________________________________________________________
-void KEYCHECK(void)
+void KEYCHECK(void){
+	if (p==0)
 {
+	//LCD_Cmd(0xc0);
+}
 	switch(value)
 	{
 		case 0:
 		LCD_String("7");
 		LCD_Cmd(0x14);
 		press=7;
-		
+		KEY_Count++;
 		break;
+		
 		case 1:
 		LCD_String("8");
 		LCD_Cmd(0x14);
 		press=8;
+		KEY_Count++;
 		break;
+		
 		case 2:
 		LCD_String("9");
 		press=9;
 		LCD_Cmd(0x14);
+		KEY_Count++;
 		break;
+		
 		case 3:
 		LCD_String("/");
 		LCD_Cmd(0x14);
 		break;
+		
 		case 4:
 		LCD_String("4");
 		LCD_Cmd(0x14);
 		press=4;
+		KEY_Count++;
 		break;
+		
 		case 5:
 		LCD_String("5");
 		LCD_Cmd(0x14);
 		press=5;
+		KEY_Count++;
 		break;
+		
 		case 6:
 		LCD_String("6");
 		LCD_Cmd(0x14);
 		press=6;
+		KEY_Count++;
 		break;
+		
 		case 7:
-		//LCD_String("*");
-		//LCD_Cmd(0x14);
 		press=15;
+		KEY_Count++;
 		break;
+		
 		case 8:
 		LCD_String("1");
 		LCD_Cmd(0x14);
 		press=1;
+		KEY_Count++;
 		break;
+		
 		case 9:
 		LCD_String("2");
 		LCD_Cmd(0x14);
 		press=2;
+		KEY_Count++;
 		break;
+		
 		case 10:
 		LCD_String("3");
 		LCD_Cmd(0x14);
 		press=3;
+		KEY_Count++;
 		break;
+		
 		case 11:
 		LCD_String("-");
 		LCD_Cmd(0x14);
 		break;
+		
 		case 12:
 		Change_Password();
+		KEY_Count = 4;
 		break;
+		
 		case 13:
 		LCD_String("0");
 		LCD_Cmd(0x14);
 		press=0;
+		KEY_Count++;
 		break;
+		
 		case 14:
-		//LCD_String("=");
 		press=14;
+		KEY_Count++;
 		break;
+		
 		case 15:
 		LCD_String("+");
 		break;
@@ -283,44 +310,61 @@ void TILT_ANGLE(void)
 	}
 	
 
-
 void COMPARE_PASSWORD(void){  
 	Read_EEPROM_Password();
-	  Safe_Status=0;
+	  Safe_Status=1;
 	if ((Input_Password[0]==Actual_Password[0]) && (Input_Password[1]==Actual_Password[1]) && (Input_Password[2]==Actual_Password[2]) && (Input_Password[3]==Actual_Password[3])){
-		password_count++;
-		BIRG(1);//Flash the BIRG Green
-	}
+		BIRG(3);//Flash the BIRG Green 5 times
+		if (Safe_Status!=2){Safe_Status=0; main();}
+	    }else{
+			 Safe_Status=0; main();
+			 }
 }
 
 void Change_Password(){ 
 	if(Safe_Status==2){ //Allows for change password if only the safe is open
+		Safe_Status=4;
+		p=0;
 		LCD_Clear();
 		LCD_String("RESET MODE");
 		_delay_ms(1000);
 		LCD_Clear();
 		LCD_String("OLD PASSCODE:");
 		LCD_Cmd(0XC0);
-			// Read the entered password from the keypad
-			for(int p=0;p<=4;p++){
-				value = PINB;
-				KEYCHECK();
-				Input_Password[p]=press;
-				_delay_ms(1000);
-		       }
-			// Display the entered password from the keypad   
-				 LCD_String(":");
-				 for (int i=0;i<=3;i++)
-				 { itoa (Input_Password[i],buffer,10);
-					 LCD_String(buffer);
-					 _delay_ms(100);
-				 }
+		LCD_String(":");
+			// Read the entered OLD password from the keypad
+		for(int z=0;z<=100000;z++){
+			if (p>=4){break;}
+		}
+			if ((Input_Password[0]==Actual_Password[0]) && (Input_Password[1]==Actual_Password[1]) && (Input_Password[2]==Actual_Password[2]) && (Input_Password[3]==Actual_Password[3])){
+				
+						p=0;
+						LCD_Clear();
+						LCD_String("NEW PASSCODE:");
+						LCD_Cmd(0XC0);
+						// Read the entered NEW password from the keypad
+						for(int z=0;z<=10000000;z++){
+							if (p>=4){break;}
+							}
+							p=0;//wait for p=4 thus 5 key interupts
+			         // Display the entered password from the keypad
+						 LCD_Clear(); 
+						LCD_String("NEW PASSCODE");
+						 LCD_Cmd(0XC0);
+						 LCD_String(":");
+						 for (int i=0;i<=3;i++)
+						 { itoa (Input_Password[i],buffer,10);
+							 LCD_String(buffer);
+							 _delay_ms(100);
+						 }
+				Store_password(); //store the password in eeprom
+			} else{Safe_Status=2;}
 	 
-	 Store_password(); //store the password in eeprom
+	 
 	
-}
+ }
 		
-	}
+}
 	
 
 
